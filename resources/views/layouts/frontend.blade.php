305c202e5111713f1d848b3e3d264024de082ec9
Carlo Bactol <you@example.com>
2022-04-14 06:15:25 +0800

    Update Name

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MunicipalityOfTalusan') }}</title>
 
     <link rel="shortcut icon" type="img/png" href="{{ asset('frontend/img/favicon.png') }}">
 
